ddc0c9309b8191482b63cb35278715c98232fd60
Alfonso Andrés López Molina <osnoser_12@hotmail.com>
2016-08-20 12:07:36 -0400

    - Funcionamiento muy básico de angular 2.

--- Angular2Example/src/app/app.component.ts
+++ Angular2Example/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Libro {
+    idLibro: number;
+    titulo: string;
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'la aplicacion trabaja!';
+    libro: Libro = {
+        idLibro: 1,
+        titulo: "Introduccion a Javascript",
+    };
 }
